bd0efeabb282b89caed446eb5d9ef6658b3f1e63
AlexTurboG <alexander.geo.georgiev@gmail.com>
2025-04-30 23:26:37 +0300

    Added things that dont work yet
    added manual get and post for the workouts. added the fitness_tracker app. Added workout model, updating the urls in progress. adding the deleting, seeing and updating the workout soon.

---
+++ tracker/fitness_tracker/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Workout(models.Model):
+    WORKOUT_TYPES = [
+        ('cardio', 'Cardio'),
+        ('weight_strength', 'Weight-Strength'),
+        ('calisthenics', 'Calisthenics')
+        ('stretching', 'Stertching'),
+    ]
+
+    title = models.CharField(max_length=255)
+    workout_type = models.CharField(max_length=20,choices=WORKOUT_TYPES)
+    duration_minutes = models.PositiveIntegerField()
+    date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
